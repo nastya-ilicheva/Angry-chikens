218b9aa8863041c5a352d7ae2647bb5e9168d72c
Nastya <nastya.v18rt@gmail.com>
2023-12-26 17:36:56 +0400

    добавила класс кнопки и создание нового окна

--- basicwindow.py
+++ basicwindow.py
@@ -1,40 +1,25 @@
 import pygame
 import sys
-from button import Button
-from basicwindow import NewWindow
 
 
-class StartWindow:
+class NewWindow:
     def __init__(self):
         pygame.init()
         self.fon = pygame.image.load("snow.jpg")
 
-    def background_image(self):
+
+    def run(self):
         self.fon = pygame.transform.scale(self.fon, (1300, 750))
         self.image = self.fon
         self.width, self.height = self.image.get_width(), self.image.get_height()
         self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption("angry chikens")
+        pygame.display.set_caption("new")
 
-        while True:
+        running = True
+        while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
             self.screen.blit(self.image, (0, 0))
-
-            # создание кнопок
-            button = Button(20, 20, 50, 50, "dfjh", self.screen, self.open_new_window)
-            button.draw()
-
-            pygame.display.update()
-
-    def open_new_window(self):
-        new_window = NewWindow()
-        new_window.run()
-
-
-if __name__ == "__main__":
-    window = StartWindow()
-    window.background_image()
+            pygame.display.flip()
