2218fc2a75541377da8ceaea5fdb077f9fe48a5e
Nastya <nastya.v18rt@gmail.com>
2024-01-30 16:44:32 +0400

    катапульта=)

--- b2/catapult.py
+++ b2/catapult.py
@@ -6,14 +6,14 @@ import util
 from math import sin
 
 from Box2D.b2 import world, polygonShape, circleShape, staticBody, dynamicBody
-from Box2D import b2Color, b2MouseJointDef
+from Box2D import b2Color, b2MouseJointDef, b2RopeJointDef
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Catapult")
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 
-world = world(gravity=(0, 0))
+world = world(gravity=(0, -25))
 
 util.screen = screen
 polygonShape.draw = util.my_draw_polygon
@@ -31,7 +31,7 @@ center_body = world.CreateStaticBody(
 center_body1 = world.CreateStaticBody(position=(0, 0))
 center_body1.CreateCircleFixture(radius=3, density=1, friction=0.3)
 
-joint = world.CreateMotorJoint(bodyA=ball_body, bodyB=center_body, maxForce=100, maxTorque=1000)
+joint = world.CreateMotorJoint(bodyA=ball_body, bodyB=center_body, maxForce=10000, maxTorque=1000000)
 
 mJoint = world.CreateMouseJoint(bodyA=center_body,
                                 bodyB=ball_body,
@@ -39,11 +39,20 @@ mJoint = world.CreateMouseJoint(bodyA=center_body,
                                 maxForce=5000000.0)
 
 
+rope = world.CreateJoint(b2RopeJointDef(
+                                    bodyA=ball_body,
+                                    bodyB=center_body,
+                                    maxLength=20,
+                                    localAnchorA=(0, 0),
+                                    localAnchorB=(0, 0)))
+
+
 def create_ball(position):
     ball_body = world.CreateDynamicBody(position=position)
     ball_body.CreateCircleFixture(radius=5, density=1, friction=0.3, restitution=1)
     Ball(all_sprites, ball_body, scale=True)
 
+flag = True
 
 running = True
 moving = False
@@ -59,7 +68,17 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
             moving = False
             world.DestroyBody(center_body1)
+            world.DestroyJoint(rope)
+            world.DestroyJoint(mJoint)
 
+
+    if flag:
+        print(ball_body.position, center_body.position)
+        if (ball_body.position.x - center_body.position.x) ** 2 + (ball_body.position.y - center_body.position.y) ** 2 < 4:
+            print(1)
+            world.DestroyJoint(joint)
+            world.DestroyBody(center_body)
+            flag = False
     screen.fill((0, 0, 0, 0))
     world.Step(TIME_STEP, 10, 10)
     util.draw_bodies(world)
