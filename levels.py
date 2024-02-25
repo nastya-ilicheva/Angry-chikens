449a2b46945276261c4c525bdf81a66c30d568df
Sofi-s <122690888+Sofi-s@users.noreply.github.com>
2024-02-25 17:40:58 +0300

    4

--- levels.py
+++ levels.py
@@ -85,3 +85,31 @@ def level_3(world, all_sprites, bird_sprites):
 
     bird = FlyBird(world, bird_sprites, center_body, "data/yellow_hen.png")
     return center_body, bird, rat, 3, "data/yellow_hen.png"
+
+
+def level_4(world, all_sprites, bird_sprites):
+    bar_body = world.CreateStaticBody(position=(29, -28), shapes=polygonShape(box=(30, 1)))
+    Brick(all_sprites, bar_body)  # пол
+
+    brick_body = world.CreateDynamicBody(position=(21, -20))
+    brick_body.CreatePolygonFixture(box=(5.5, 9), density=9, friction=1)
+    Brick(all_sprites, brick_body)  # правая стена
+
+    brick_body = world.CreateDynamicBody(position=(30, -20))
+    brick_body.CreatePolygonFixture(box=(10, 9), density=5, friction=0.8)
+    Brick(all_sprites, brick_body)  # левая стена
+
+    brick_body = world.CreateDynamicBody(position=(28, -15))
+    brick_body.CreatePolygonFixture(box=(2, 21), density=10, friction=1)
+    Brick(all_sprites, brick_body)  # крыша
+    #
+    ball_body = world.CreateDynamicBody(position=(29, -8))
+    ball_body.CreateCircleFixture(radius=6, density=15, friction=1, restitution=0.8)
+    rat = Ball(all_sprites, ball_body, scale=True)
+
+    center_body = world.CreateStaticBody(
+        position=(-40, -20),
+        shapes=polygonShape(box=(0.2, 0.2)))
+
+    bird = FlyBird(world, bird_sprites, center_body, "data/yellow_hen.png")
+    return center_body, bird, rat, 3, "data/blue_hen.png"
